54c3d12c472e2ea33b2956c9e97c8e0c242c1527
Jonathan Vasquez <vasqjr@farmingdale.edu>
2025-09-22 22:05:10 -0400

    I don't even know what I did here to be honest

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner from './components/Banner.tsx';
-import bannerImage from '/src/assets/images/IMG_3541.jpeg'
-import './App.css'
+import bannerImage from '/src/assets/images/IMG_3541.jpeg';
+import './App.css';
 
 function App() {
 
@@ -24,42 +24,25 @@ function App() {
           About Me
         </h2>
         <p className='smallIndent'>
-          What could I say that just defines me as me, not as a front-end engineer. Biggest thing you should know about me is while I don't mind an occasional relaxation day, I find comfort in knowing I got something done at the end of the day. This has led to me being a consistent moving person who tries to accomplish something before I go back to bed. Whether it's working on a project such as this website, making progress in a song I'm practicing on piano, or my personal favorite hobby of having got a gym session in, I can relax when I feel like I've come out of the day a little better than I was when I woke up. Maybe it sounds cliche but it truly is just how I feel. I understand life only can happen as fast as one day at a time, but to me it's what you do in those days that really matters.
-        </p>
+          I’m happiest when I finish the day having accomplished something. Whether it’s refining this website, recording for my YouTube channel, practicing piano, or getting a gym session in, I like to end each day a little better than I started. Relaxation has its place, but steady progress—one day at a time—is what matters most to me.</p>
       </div>
 
       <div id="workExperienceSection">
         <h2>Education & Work Experience</h2>
 
         <div className='smallIndent'>
-          <p> If I had been passionate about computing ever since I was a child, I would put it here, but in all honesty my coding story starts
-            back in 2020. I've always enjoyed messing around with web pages and video games but never thought much of it past that. I actively remember my first time using
-            inspect element back in elementary school to mess with the coloring and pixel sizes of websites I frequented, but it wasn't until I
-            started my Computer Engineering degree (Yup, not Computer Science) in 2020 that I finally figured out what I wanted to do in life.</p>
           <h3>Bachelor of Science in Computer Science at SUNY Farmingdale</h3>
           <div className="leftSideImage">
             <img className="square-container" src='src/assets/images/college-grad.jpeg' alt="Photo of Jonathan with College Graduate Friends" />
           </div>
           <div>
-            <h4>2020-2021</h4>
+            <h4>2020-2024</h4>
             <p>
-                The Covid-19 Pandemic of 2020 was an unpresidented time, for all of us. I personally felt most affected in my studies,
-              having graduated high school and started my Computer Engineering degree online. I attempted to make the most of my first
-              year of college but struggled to work online in a major dominated by hardware and circuitry. It wasn't until my 3rd semester,
-              where I finally could take an in person class on C++ where I realize just how fun coding could be.
-            </p>
-            <h4>2022-2024</h4>
-            <p> After the C++ class, I switched over to Computer Science in Spring of 2022. It was during a
-              summer programming class where I met a great group of friends whom I would eventually take most of
-              my major classes with. This got to a point where our study group became well-known throughout the Computer Scientist professors
-              as we were often seen in multiple of their classes during the day or studying at the library.
-              During this time I also had been a member of the Supporting Women in Computing club and the Competitive E-Sports team.
-              In Spring 2024, I graduated with these people and finally earned my B.S. in Computer Science after years of hardwork.
+                
             </p>
           </div>
         </div>
       </div>
-
     </>
   )
 }
